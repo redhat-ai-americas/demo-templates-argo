8ae720d3ce046c0337dac677dd5ae14810e4cf91
Brian Ball <briball@umich.edu>
2026-01-08 08:58:49 -0500

    Recreate the secret if it already exists.

--- helm/cluster-url-getter/files/create_secret.sh
+++ helm/cluster-url-getter/files/create_secret.sh
@@ -4,5 +4,9 @@ if oc get secret cluster-url >/dev/null 2>&1; then
   oc delete secret cluster-url
 fi
 oc create secret generic cluster-url --from-literal=BASE_URL="$BASE_URL"
+
+if oc get secret my-token >/dev/null 2>&1; then
+  oc delete secret my-token
+fi
 TOKEN=$(oc whoami -t)
 oc create secret generic my-token --from-literal=TOKEN=$TOKEN
